f7a867d162cea35b88673de6a0b27a0c7f4c915a
Enoch Klufio <enochmk94@gmail.com>
2025-05-26 19:24:46 +0000

    feat: implement sign-in functionality with validation and integrate sign-in form into login page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,12 @@
 import { AppSidebar } from '@/components/app-sidebar';
-import { SectionCards } from '@/components/section-cards';
 import { SiteHeader } from '@/components/site-header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
-export default function Page() {
+export default function MainLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	return (
 		<SidebarProvider
 			style={
@@ -16,13 +19,7 @@ export default function Page() {
 			<AppSidebar variant='inset' />
 			<SidebarInset>
 				<SiteHeader />
-				<div className='flex flex-1 flex-col'>
-					<div className='@container/main flex flex-1 flex-col gap-2'>
-						<div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
-							<SectionCards />
-						</div>
-					</div>
-				</div>
+				{children}
 			</SidebarInset>
 		</SidebarProvider>
 	);
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { IconCirclePlusFilled, IconMail, type Icon } from '@tabler/icons-react';
-
-import { Button } from '@/components/ui/button';
+import { type Icon } from '@tabler/icons-react';
 import {
 	SidebarGroup,
 	SidebarGroupContent,
@@ -10,6 +8,7 @@ import {
 	SidebarMenuButton,
 	SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 export function NavMain({
 	items,
@@ -25,12 +24,17 @@ export function NavMain({
 			<SidebarGroupContent className='flex flex-col gap-2'>
 				<SidebarMenu>
 					{items.map((item) => (
-						<SidebarMenuItem key={item.title}>
-							<SidebarMenuButton tooltip={item.title}>
-								{item.icon && <item.icon />}
-								<span>{item.title}</span>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
+						<Link
+							href={item.url}
+							key={item.title}
+						>
+							<SidebarMenuItem key={item.title}>
+								<SidebarMenuButton tooltip={item.title}>
+									{item.icon && <item.icon />}
+									<span>{item.title}</span>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						</Link>
 					))}
 				</SidebarMenu>
 			</SidebarGroupContent>
