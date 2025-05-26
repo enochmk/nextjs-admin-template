858ddb555c959b377e33772cb58c31ce04f35912
Enoch Klufio <enochmk94@gmail.com>
2025-05-26 14:48:14 +0000

    feat: implement sign-in form with validation and integrate into login page

---
+++ app/login/components/sign-in-form.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+
+const formSchema = z.object({
+	username: z.string().min(2, {
+		message: 'Username must be at least 2 characters.',
+	}),
+	password: z.string().min(6, {
+		message: 'Password must be at least 6 characters.',
+	}),
+});
+
+export default function SignInForm({
+	className,
+	...props
+}: React.ComponentProps<'div'>) {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: '',
+			password: '',
+		},
+	});
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		console.log(values);
+	}
+
+	return (
+		<div
+			className={cn('flex flex-col gap-6', className)}
+			{...props}
+		>
+			<Card>
+				<CardHeader className='text-center'>
+					<CardTitle className='text-xl'>Welcome</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className='space-y-8'
+						>
+							<div className='grid gap-4'>
+								<FormField
+									control={form.control}
+									name='username'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Username</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='shadcn'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name='password'
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Password</FormLabel>
+											<FormControl>
+												<Input
+													placeholder='********'
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<Button
+									type='submit'
+									className='w-full'
+								>
+									Sign In
+								</Button>
+							</div>
+						</form>
+					</Form>
+				</CardContent>
+			</Card>
+
+			<div className='text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4'>
+				By clicking continue, you agree to our <a href='#'>Terms of Service</a>{' '}
+				and <a href='#'>Privacy Policy</a>.
+			</div>
+		</div>
+	);
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import SignInForm from '@/app/login/components/sign-in-form';
+
+export default function LoginPage() {
+	return (
+		<div className='bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
+			<div className='flex w-full max-w-sm flex-col gap-6'>
+				<SignInForm />
+			</div>
+		</div>
+	);
+}
