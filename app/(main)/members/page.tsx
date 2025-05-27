26376fa39ddc6d5534ec37d1f31e6b09e4134195
Enoch Klufio <enochmk94@gmail.com>
2025-05-27 10:17:53 +0000

    feat: update member addition link to include icon and change text to 'Add Member'

--- app/(main)/members/page.tsx
+++ app/(main)/members/page.tsx
@@ -9,6 +9,7 @@ import {
 import Link from 'next/link';
 import { db } from '@/lib/db';
 import { members } from '@/lib/db/schema';
+import { PlusIcon } from 'lucide-react';
 
 export default async function MembersPage() {
 	const membersList = await db.select().from(members);
@@ -20,7 +21,8 @@ export default async function MembersPage() {
 					href='/members/new'
 					className='bg-primary hover:bg-primary/90 text-primary-foreground px-4 py-2 rounded inline-block'
 				>
-					Add New Member
+					<PlusIcon className='inline mr-1' />
+					Add Member
 				</Link>
 			</div>
 			<Table>
