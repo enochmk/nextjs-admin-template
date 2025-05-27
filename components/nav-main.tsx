c5594564e253876194841956d55c7718811d62f2
Enoch Klufio <enochmk94@gmail.com>
2025-05-27 11:03:21 +0000

    feat: enhance NavMain component to include active state for menu items

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -9,6 +9,7 @@ import {
 	SidebarMenuItem,
 } from '@/components/ui/sidebar';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export function NavMain({
 	items,
@@ -19,6 +20,8 @@ export function NavMain({
 		icon?: Icon;
 	}[];
 }) {
+	const pathname = usePathname();
+
 	return (
 		<SidebarGroup>
 			<SidebarGroupContent className='flex flex-col gap-2'>
@@ -29,7 +32,10 @@ export function NavMain({
 							key={item.title}
 						>
 							<SidebarMenuItem key={item.title}>
-								<SidebarMenuButton tooltip={item.title}>
+								<SidebarMenuButton
+									tooltip={item.title}
+									isActive={pathname === item.url}
+								>
 									{item.icon && <item.icon />}
 									<span>{item.title}</span>
 								</SidebarMenuButton>
